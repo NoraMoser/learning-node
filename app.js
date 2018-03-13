ca39d8224d5faa70f52bde30bffa5cda8b8ef7b8
Moser <noralmoser@gmail.com>
2018-03-13 15:16:27 -0500

    started using lodash and nodemon

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ console.log('starting app');
 
 const fs = require('fs');
 const os = require('os');
+const _ = require('lodash');
 const notes = require('./notes.js');
 
 // var user = os.userInfo();
@@ -10,8 +11,14 @@ const notes = require('./notes.js');
 // fs.appendFileSync('greetings.txt', 'Hello' + user.username + '!');
 // fs.appendFileSync('greetings.txt', `Hello ${user.username}! You are ${notes.age}`);
 
-var res = notes.addNote();
-console.log(res);
+// var res = notes.addNote();
+// console.log(res);
 
-var addNumber = notes.add(1, 2);
-console.log(addNumber);
+// var addNumber = notes.add(1, 2);
+// console.log(addNumber);
+
+// console.log(_.isString(true));
+// console.log(_.isString('Nora'));
+
+var filteredArray = _.uniq(['Mike', 'Nora', 3, 5, 'Nora', 5, 6, 7, 'Nora']);
+console.log(filteredArray);
--- notes.js
+++ notes.js
@@ -7,6 +7,6 @@ module.exports.addNote = () => {
     return 'New note';
 };
 
-module.exports.add = (a, b) => {
-    return a + b;
-}
+// module.exports.add = (a, b) => {
+//     return a + b;
+// }
