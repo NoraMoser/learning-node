ea7ec32d045fb3c48b3a23ea5bcd6a799ae22c2f
Moser <noralmoser@gmail.com>
2018-03-16 11:24:06 -0500

    fs stuff and if statements

--- notes.js
+++ notes.js
@@ -10,9 +10,31 @@ console.log('notes page');
 // module.exports.add = (a, b) => {
 //     return a + b;
 // }
+const fs = require('fs');
 
 var addNote = (title, body) => {
-    console.log('Adding note', title, body);
+    var notes = [];
+    var note = {
+        title,
+        body
+    };
+
+    try {
+        var notesString = fs.readFileSync('notes-data.json');
+        notes = JSON.parse(notesString);
+    } catch (e) {
+
+    }
+
+    var duplicateNotes = notes.filter((note) => note.title === title);
+
+    if (duplicateNotes.length === 0) {
+        notes.push(note);
+        fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+    }
+
+
+
 };
 
 var getAll = () => {
